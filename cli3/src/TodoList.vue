158c964def61654bca783acd98a09ec223bceb27
mfxy <mfxyzm@outlook.com>
2018-07-17 06:04:00 +1000

    Vue TodoList with cli 3

---
+++ cli3/src/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input class="form-control" placeholder="New Item" v-model="inputValue"/>
+        <button class="btn btn-primary" @click="handleSubmit">Submit</button>
+        <ul class="list-group">
+          <TodoItem
+            class="list-group-item list-group-item-secondary" 
+            v-for="(item,index) of array" 
+            :key="index"
+            :content="item"
+            :index="index"
+            @delete-item="handleDelete"/>
+        </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'TodoList',
+  components: {
+    TodoItem
+  },
+  data: function(){
+    return {
+      inputValue:'',
+      array: []
+    }
+  },
+  methods: {
+    handleSubmit: function(){
+        this.array.push(this.inputValue);
+        this.inputValue='';
+    },
+    handleDelete: function(index){
+        this.array.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+  ul{
+    list-style: none;
+  }
+</style>
---
+++ cli3/src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <li @click="handleClick"> {{index+1}}: {{content}} </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['content','index'],
+  methods: {
+    handleClick: function(){
+        this.$emit('delete-item',this.index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
